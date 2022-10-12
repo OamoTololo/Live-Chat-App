cb1d8474f56d5963f999a3c59364997a36f7cfdb
Oamogetswe Mgidi <oamotebogomgidi@gmail.com>
2022-10-12 12:55:50 +0200

    Modified the paths to the css and js files.

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/all.min.css">
     <title>Sosha chat App</title>
 
     <script src="https://kit.fontawesome.com/590e8175ae.js" crossorigin="anonymous"></script>
@@ -49,7 +49,7 @@
         </section>
     </div>
 
-    <script src="/js/pass-show-hide.js"></script>
+    <script src="js/pass-show-hide.js"></script>
 </body>
 
 </html>
